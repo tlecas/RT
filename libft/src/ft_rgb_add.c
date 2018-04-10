2eb28324e0668c1aaec07f1f7cd2cf2be1b45ac9
Romain PAGOT <rpagot@e3r12p4.42.fr>
2018-04-11 01:01:20 +0200

    hello

--- libft/src/ft_rgb_add.c
+++ libft/src/ft_rgb_add.c
@@ -13,31 +13,45 @@
 #include <stdio.h>
 #include "libft.h"
 
-void	rgb_add(unsigned int *color, unsigned int add)
+/*void	rgb_add(unsigned int *color, unsigned int d)
 {
 	unsigned int r;
 	unsigned int g;
 	unsigned int b;
 	unsigned int inv;
 
-	inv = (*color / 0x1000000);
-	*color = *color % 0x1000000;
-	add = add % 0x1000000;
-	//printf("color = %d add = %d\n", (*color / 0x10000), (add / 0x10000));
-	r = (*color / 0x10000) + (add / 0x10000);
-	//printf("r = %d\n", r);
+	inv = (*color >> 24);
+	*color <<= 8;
+	r = (*color >> 24) + d;
 	uiclamp(&r, 0, 255);
-	*color = *color % 0x10000;
-	add = add % 0x10000;
-	//printf("color = %d add = %d\n", (*color / 0x100), (add / 0x100));
-	g = (*color / 0x100) + (add / 0x100);
-	//printf("g = %d\n", g);
+	*color <<= 8;
+	g = (*color >> 24) + d;
 	uiclamp(&g, 0, 255);
-	*color = *color % 0x100;
-	add = add % 0x100;
-	//printf("color = %d add = %d\n", *color, add);
-	b = *color + add;
-	//printf("b = %d\n", b);
+	*color <<= 8;
+	b = (*color >> 24) + d;
 	uiclamp(&b, 0, 255);
-	*color =(inv * 0x1000000) + (r * 0x10000) + (g * 0x100) + b;
+	*color = (inv << 24) + (r << 16) + (g << 8) + b;
+} BLACK WHITE FILTER*/
+
+void	rgb_add(unsigned int *color, unsigned int d)
+{
+	unsigned int r;
+	unsigned int g;
+	unsigned int b;
+	unsigned int inv;
+
+	inv = (*color >> 24);
+	*color <<= 8;
+	d <<= 8;
+	r = (*color >> 24) + (d >> 24);
+	uiclamp(&r, 0, 255);
+	*color <<= 8;
+	d <<= 8;
+	g = (*color >> 24) + (d >> 24);
+	uiclamp(&g, 0, 255);
+	*color <<= 8;
+	d <<= 8;
+	b = (*color >> 24) + (d >> 24);
+	uiclamp(&b, 0, 255);
+	*color = (inv << 24) + (r << 16) + (g << 8) + b;
 }
