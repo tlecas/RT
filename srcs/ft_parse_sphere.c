09f109e8e95c8d4f24c1b1f69e9acde46d496a21
Emile PREVOT <mprevot@e1r10p11.42.fr>
2018-05-02 11:34:59 +0200

    norminette srcs/ft_parse_sphere.c

--- srcs/ft_parse_sphere.c
+++ srcs/ft_parse_sphere.c
@@ -12,16 +12,21 @@
 
 #include "rt.h"
 
-t_sphere		*ft_init_sphere(t_sphere *sphere)
+t_bool			ft_fill_properties2(t_sphere *sphere, char *str)
 {
-	if (!(sphere = malloc(sizeof(t_sphere))))
-		ft_error("Error malloc'ing!");
-	sphere->pos = coord_v(0.0f, 0.0f, 0.0f);
-	sphere->mat = ft_mat_init(0.0f, 0.0f, 0.0f);
-	sphere->radius = 1.0f;
-	sphere->color = 0x000000FF;
-	sphere->inter = 0.0f;
-	return (sphere);
+	if (!(ft_strncmp(str, "\tambient: ", 10)))
+		sphere->mat.ambient = ft_atof_free(ft_strrcpy(str, 10));
+	else if (!(ft_strncmp(str, "\tdiffuse: ", 10)))
+		sphere->mat.diffuse = ft_atof_free(ft_strrcpy(str, 10));
+	else if (!(ft_strncmp(str, "\tspecular: ", 11)))
+		sphere->mat.specular = ft_atof_free(ft_strrcpy(str, 11));
+	else if (!(ft_strncmp(str, "\treflection: ", 13)))
+		sphere->mat.reflection = ft_atof_free(ft_strrcpy(str, 13));
+	else if (!(ft_strncmp(str, "\trefraction: ", 13)))
+		sphere->mat.refraction = ft_atof_free(ft_strrcpy(str, 13));
+	else
+		return (0);
+	return (1);
 }
 
 static int		ft_fill_properties(t_sphere *sphere, char *str)
@@ -29,28 +34,23 @@ static int		ft_fill_properties(t_sphere *sphere, char *str)
 	char	*tmp;
 	char	*test;
 
+	tmp = NULL;
 	if (!(ft_strncmp(str, "\tradius: ", 9)))
-		sphere->radius = ft_atof(tmp = ft_strrcpy(str, 9));
+		sphere->radius = ft_atof_free(ft_strrcpy(str, 9));
 	else if (!(ft_strncmp(str, "\tcolor: ", 8)))
 	{
 		errno = 0;
-		sphere->color = strtol(tmp = ft_strrcpy(str, 8), &test, 16);
-		if ((errno == ERANGE && (sphere->color == UINT_MAX || sphere->color == 0))
-            || (errno != 0 && sphere->color == 0) || '\0' != *test)
+		sphere->color = strtol(
+			tmp = ft_strrcpy(str, 8), &test, 16);
+		if ((errno == ERANGE &&
+			(sphere->color == UINT_MAX || sphere->color == 0))
+			|| (errno != 0 && sphere->color == 0) || '\0' != *test)
 		{
 			ft_error("Invalid color");
 		}
 	}
-	else if (!(ft_strncmp(str, "\tambient: ", 10)))
-		sphere->mat.ambient = ft_atof(tmp = ft_strrcpy(str, 10));
-	else if (!(ft_strncmp(str, "\tdiffuse: ", 10)))
-		sphere->mat.diffuse = ft_atof(tmp = ft_strrcpy(str, 10));
-	else if (!(ft_strncmp(str, "\tspecular: ", 11)))
-		sphere->mat.specular = ft_atof(tmp = ft_strrcpy(str, 11));
-	else if (!(ft_strncmp(str, "\treflection: ", 13)))
-		sphere->mat.reflection = ft_atof(tmp = ft_strrcpy(str, 13));
-	else if (!(ft_strncmp(str, "\trefraction: ", 13)))
-		sphere->mat.refraction = ft_atof(tmp = ft_strrcpy(str, 13));
+	else if (ft_fill_properties2(sphere, str))
+		(void)tmp;
 	else
 		return (0);
 	free(tmp);
@@ -59,32 +59,30 @@ static int		ft_fill_properties(t_sphere *sphere, char *str)
 
 static int		ft_fill_coords(t_sphere *sphere, char *str)
 {
-	char	*tmp;
-
 	if (!(ft_strncmp(str, "\tx: ", 4)))
-		sphere->pos.x = ft_atof(tmp = ft_strrcpy(str, 4));
+		sphere->pos.x = ft_atof_free(ft_strrcpy(str, 4));
 	else if (!(ft_strncmp(str, "\ty: ", 4)))
-		sphere->pos.y = ft_atof(tmp = ft_strrcpy(str, 4));
+		sphere->pos.y = ft_atof_free(ft_strrcpy(str, 4));
 	else if (!(ft_strncmp(str, "\tz: ", 4)))
-		sphere->pos.z = ft_atof(tmp = ft_strrcpy(str, 4));
+		sphere->pos.z = ft_atof_free(ft_strrcpy(str, 4));
 	else
 		return (0);
-	free(tmp);
 	return (1);
 }
 
-static t_sphere		*ft_parse_properties(t_sphere *sphere, char *str)
+static t_sphere	*ft_parse_properties(t_sphere *sphere, char *str)
 {
 	if (str && str[0] == '\t')
 	{
-		if ((!ft_fill_coords(sphere, str)) && (!ft_fill_properties(sphere, str)))
+		if ((!ft_fill_coords(sphere, str)) &&
+			(!ft_fill_properties(sphere, str)))
 			ft_error("Can't parse properties of an object");
 		return (sphere);
 	}
 	return (0);
 }
 
-int		ft_parse_sphere(t_env *e, char **tab)
+int				ft_parse_sphere(t_env *e, char **tab)
 {
 	int		i;
 	int		j;
@@ -100,7 +98,8 @@ int		ft_parse_sphere(t_env *e, char **tab)
 		if (!(ft_strcmp(tab[i], "sphere:")))
 		{
 			++j;
-			e->sphere[j] = ft_init_sphere(e->sphere[j]);
+			if (!(e->sphere[j] = ft_memalloc(sizeof(t_sphere))))
+				ft_error("Error malloc'ing!");
 			insphere = 1;
 		}
 		else if (insphere == 1 && tab[i][0] == '\t')
