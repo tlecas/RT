4cf8a60cf474f439262cbe972082b3440fab06b9
Emile PREVOT <mprevot@e1r10p11.42.fr>
2018-05-02 12:01:20 +0200

    srcs/ft_parse_para.c to norme

--- srcs/ft_parse_para.c
+++ srcs/ft_parse_para.c
@@ -12,71 +12,53 @@
 
 #include "rt.h"
 
-t_para		*ft_init_para(t_para *para)
-{
-	if (!(para = malloc(sizeof(t_para))))
-		ft_error("Error malloc!");
-	para->pos = coord_v(0.0f, 0.0f, 0.0f);
-	para->normal = coord_v(0.0f, 0.0f, 0.0f);
-	para->k = 0;
-	para->color = 0x0000FFFF;
-	para->mat = ft_mat_init();
-	para->inter = 0;
-	return (para);
-}
-
 static int		ft_fill_properties(t_para *para, char *str)
 {
 	char	*tmp;
 	char	*test;
 
 	if (!(ft_strncmp(str, "\tambient: ", 10)))
-		para->mat.ambient = ft_atof(tmp = ft_strrcpy(str, 10));
-	else if (!(ft_strncmp(str, "\tcolor: ", 8)))
+		para->mat.ambient = ft_atof_free(ft_strrcpy(str, 10));
+	else if (!(ft_strncmp(str, "\tcolor: ", 8)) && !(errno = 0))
 	{
-		errno = 0;
-		para->color = strtol(tmp = ft_strrcpy(str, 8), &test, 16);
+		para->color = strtol(
+			tmp = ft_strrcpy(str, 8), &test, 16);
+		free(tmp);
 		if ((errno == ERANGE && (para->color == UINT_MAX || para->color == 0))
-            || (errno != 0 && para->color == 0) || '\0' != *test)
-		{
+			|| (errno != 0 && para->color == 0) || '\0' != *test)
 			ft_error("Invalid color");
-		}
 	}
 	else if (!(ft_strncmp(str, "\tdiffuse: ", 10)))
-		para->mat.diffuse = ft_atof(tmp = ft_strrcpy(str, 10));
+		para->mat.diffuse = ft_atof_free(ft_strrcpy(str, 10));
 	else if (!(ft_strncmp(str, "\tspecular: ", 11)))
-		para->mat.specular = ft_atof(tmp = ft_strrcpy(str, 11));
+		para->mat.specular = ft_atof_free(ft_strrcpy(str, 11));
 	else if (!(ft_strncmp(str, "\treflection: ", 13)))
-		para->mat.reflection = ft_atof(tmp = ft_strrcpy(str, 13));
+		para->mat.reflection = ft_atof_free(ft_strrcpy(str, 13));
 	else if (!(ft_strncmp(str, "\trefraction: ", 13)))
-		para->mat.refraction = ft_atof(tmp = ft_strrcpy(str, 13));
+		para->mat.refraction = ft_atof_free(ft_strrcpy(str, 13));
 	else
 		return (0);
-	free(tmp);
 	return (1);
 }
 
 static int		ft_fill_coords(t_para *para, char *str)
 {
-	char	*tmp;
-
 	if (!(ft_strncmp(str, "\tx: ", 4)))
-		para->pos.x = ft_atof(tmp = ft_strrcpy(str, 4));
+		para->pos.x = ft_atof_free(ft_strrcpy(str, 4));
 	else if (!(ft_strncmp(str, "\ty: ", 4)))
-		para->pos.y = ft_atof(tmp = ft_strrcpy(str, 4));
+		para->pos.y = ft_atof_free(ft_strrcpy(str, 4));
 	else if (!(ft_strncmp(str, "\tz: ", 4)))
-		para->pos.z = ft_atof(tmp = ft_strrcpy(str, 4));
+		para->pos.z = ft_atof_free(ft_strrcpy(str, 4));
 	else if (!(ft_strncmp(str, "\tnormalX: ", 10)))
-		para->normal.x = ft_atof(tmp = ft_strrcpy(str, 10));
+		para->normal.x = ft_atof_free(ft_strrcpy(str, 10));
 	else if (!(ft_strncmp(str, "\tnormalY: ", 10)))
-		para->normal.y = ft_atof(tmp = ft_strrcpy(str, 10));
+		para->normal.y = ft_atof_free(ft_strrcpy(str, 10));
 	else if (!(ft_strncmp(str, "\tnormalZ: ", 10)))
-		para->normal.z = ft_atof(tmp = ft_strrcpy(str, 10));
+		para->normal.z = ft_atof_free(ft_strrcpy(str, 10));
 	else if (!(ft_strncmp(str, "\tscalar: ", 9)))
-		para->k = ft_atof(tmp = ft_strrcpy(str, 9));
+		para->k = ft_atof_free(ft_strrcpy(str, 9));
 	else
 		return (0);
-	free(tmp);
 	return (1);
 }
 
@@ -107,7 +89,8 @@ int		ft_parse_para(t_env *e, char **tab)
 		if (!ft_strcmp(tab[i], "paraboloid:"))
 		{
 			++j;
-			e->para[j] = ft_init_para(e->para[j]);
+			if (!(e->para[j] = ft_memalloc(sizeof(t_para))))
+				ft_error("Error malloc!");
 			inpara = 1;
 		}
 		else if (inpara == 1 && tab[i][0] == '\t')
