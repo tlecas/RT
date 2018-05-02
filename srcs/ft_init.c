bd42a3e472642690bf5285d2e69fbd6649e538eb
Émile PRÉVOT <user@MacBook-Pro-de-Emile.local>
2018-05-03 00:41:29 +0200

    foret de if sur les objets

--- srcs/ft_init.c
+++ srcs/ft_init.c
@@ -52,7 +52,95 @@ void			ft_init_env(t_env *e)
 	e->screenshot = 0;
 }
 
-t_env			*ft_init(char *filename)
+void		ft_checksubvalues(t_env *e)
+{
+	int i = 0;
+
+	while (e->plane[i])
+	{
+		if (e->plane[i]->mat.ambient < 0 || e->plane[i]->mat.ambient > 1)
+			ft_error("ambient dans plane", 0, 0);
+		if (e->plane[i]->mat.diffuse < 0 || e->plane[i]->mat.diffuse > 1)
+			ft_error("diffuse dans plane", 0, 0);
+		if (e->plane[i]->mat.specular < 0 || e->plane[i]->mat.specular > 20)
+			ft_error("specular dans plane", 0, 0);
+		if (e->plane[i]->mat.reflection < 0 || e->plane[i]->mat.reflection > 1)
+			ft_error("reflection dans plane", 0, 0);
+		if (e->plane[i]->mat.refraction != 0 && (e->plane[i]->mat.refraction < 1 || e->plane[i]->mat.refraction > 2.5))
+			ft_error("refraction dans plane", 0, 0);
+		i++;
+	}
+	i = 0;
+	while (e->cone[i])
+	{
+		if (e->cone[i]->angle < 0 || e->cone[i]->angle > 90)
+			ft_error("angle dans cone", 0, 0);
+		if (e->cone[i]->mat.ambient < 0 || e->cone[i]->mat.ambient > 1)
+			ft_error("ambient dans cone", 0, 0);
+		if (e->cone[i]->mat.diffuse < 0 || e->cone[i]->mat.diffuse > 1)
+			ft_error("diffuse dans cone", 0, 0);
+		if (e->cone[i]->mat.specular < 0 || e->cone[i]->mat.specular > 20)
+			ft_error("specular dans cone", 0, 0);
+		if (e->cone[i]->mat.reflection < 0 || e->cone[i]->mat.reflection > 1)
+			ft_error("reflection dans cone", 0, 0);
+		if (e->plane[i]->mat.refraction != 0 && (e->plane[i]->mat.refraction < 1 || e->plane[i]->mat.refraction > 2.5))
+			ft_error("refraction dans cone", 0, 0);
+		i++;
+
+	}
+	i = 0;
+	while (e->cylinder[i])
+	{
+		if (!(e->cylinder[i]->radius > 0))
+			ft_error("cylinder dans cylinder", 0, 0);
+		if (e->cylinder[i]->mat.ambient < 0 || e->cylinder[i]->mat.ambient > 1)
+			ft_error("ambient dans cylinder", 0, 0);
+		if (e->cylinder[i]->mat.diffuse < 0 || e->cylinder[i]->mat.diffuse > 1)
+			ft_error("diffuse dans cylinder", 0, 0);
+		if (e->cylinder[i]->mat.specular < 0 || e->cylinder[i]->mat.specular > 20)
+			ft_error("specular dans cylinder", 0, 0);
+		if (e->cylinder[i]->mat.reflection < 0 || e->cylinder[i]->mat.reflection > 1)
+			ft_error("reflection dans cylinder", 0, 0);
+		if (e->plane[i]->mat.refraction != 0 && (e->plane[i]->mat.refraction < 1 || e->plane[i]->mat.refraction > 2.5))
+			ft_error("refraction dans cylinder", 0, 0);
+		i++;
+	}
+	i = 0;
+	while (e->sphere[i])
+	{
+		if (e->sphere[i]->radius < 0)
+			ft_error("sphere dans sphere", 0, 0);
+		if (e->sphere[i]->mat.ambient < 0 || e->sphere[i]->mat.ambient > 1)
+			ft_error("ambient dans sphere", 0, 0);
+		if (e->sphere[i]->mat.diffuse < 0 || e->sphere[i]->mat.diffuse > 1)
+			ft_error("diffuse dans sphere", 0, 0);
+		if (e->sphere[i]->mat.specular < 0 || e->sphere[i]->mat.specular > 20)
+			ft_error("specular dans sphere", 0, 0);
+		if (e->sphere[i]->mat.reflection < 0 || e->sphere[i]->mat.reflection > 1)
+			ft_error("reflection dans sphere", 0, 0);
+		if (e->plane[i]->mat.refraction != 0 && (e->plane[i]->mat.refraction < 1 || e->plane[i]->mat.refraction > 2.5))
+			ft_error("refraction dans sphere", 0, 0);
+		i++;
+	}
+	i = 0;
+}
+
+void		ft_checkvalues(t_env *e)
+{
+	if (e->width < 300 || e->width > 4000)
+		ft_error("Width value error", 0, 0);
+	if (e->height < 300 || e->height > 4000)
+		ft_error("Width value error", 0, 0);
+	if (e->recursivity < 0 || e->recursivity >= 42)
+		ft_error("Recursivity value error", 0, 0);
+	if (e->fov >= 180 || e->fov <= 45)
+		ft_error("Fov value error", 0, 0);
+	if (e->ambient_light > 1)
+		ft_error("Ambient light value error", 0, 0);
+	ft_checksubvalues(e);
+}
+
+t_env		*ft_init(char *filename)
 {
 	t_env		*e;
 	char		**tab;
@@ -78,5 +166,6 @@ t_env			*ft_init(char *filename)
 	while (tab[++i])
 		free(tab[i]);
 	free(tab);
+	ft_checkvalues(e);
 	return (e);
 }
